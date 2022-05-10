1dea98ea6f746a000b594bde2ddefd753b777b9a
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-10 16:07:52 -0700

    set up Router in App. Routes to either a ListPage or DetailPage component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getPlayerData } from './services/fetch-utils';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ListPage from './components/ListPage';
+import DetailPage from './components/DetailPage';
 
 function App() {
   const [players, setPlayers] = useState([]);
@@ -14,7 +17,21 @@ function App() {
     fetch();
   }, []);
 
-  return <div className="App">{console.log(players)}</div>;
+  return (
+    <Router>
+      <div className="App">
+        {console.log(players)}
+        <Switch>
+          <Route exact path="/">
+            <ListPage />
+          </Route>
+          <Route exact path="/player/:id">
+            <DetailPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
