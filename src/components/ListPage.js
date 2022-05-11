caef6871eca977688b37cef9038c8c64058cb918
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-10 17:24:09 -0700

    finished writing code for pagination bootstrap component - really ugly and messy

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -6,7 +6,8 @@ import PlayerList from './PlayerList';
 
 export default function ListPage() {
   const [players, setPlayers] = useState([]);
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0);
   const [paginationItems, setPaginationItems] = useState([]);
   const playersPerPage = 50;
 
@@ -16,17 +17,148 @@ export default function ListPage() {
       const to = page * playersPerPage - 1;
       const playerData = await getPlayerData(from, to);
 
-      setPlayers(playerData);
+      setPlayers(playerData.data);
+      setLastPage(Math.ceil(playerData.count / playersPerPage));
     }
     fetch();
-  }, [page]);
+  }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (page === 1) {
+      const items = (
+        <>
+          <Pagination.First key={'first'} disabled />
+          <Pagination.Prev key={'prev'} disabled />
+          <Pagination.Item key={page} active>
+            {page}
+          </Pagination.Item>
+          <Pagination.Item key={page + 1} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Pagination.Item>
+          <Pagination.Item key={page + 2} onClick={() => setPage(page + 2)}>
+            {page + 2}
+          </Pagination.Item>
+          <Pagination.Item key={page + 3} onClick={() => setPage(page + 3)}>
+            {page + 3}
+          </Pagination.Item>
+          <Pagination.Item key={page + 4} onClick={() => setPage(page + 4)}>
+            {page + 4}
+          </Pagination.Item>
+          <Pagination.Next key={'next'} onClick={() => setPage(page + 1)} />
+          <Pagination.Last key={'last'} onClick={() => setPage(lastPage)} />
+        </>
+      );
+      setPaginationItems(items);
+    }
+    if (page === 2) {
+      const items = (
+        <>
+          <Pagination.First key={'first'} onClick={() => setPage(1)} />
+          <Pagination.Prev key={'prev'} onClick={() => setPage(page - 1)} />
+          <Pagination.Item key={page - 1} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Pagination.Item>
+          <Pagination.Item key={page} active>
+            {page}
+          </Pagination.Item>
+          <Pagination.Item key={page + 1} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Pagination.Item>
+          <Pagination.Item key={page + 2} onClick={() => setPage(page + 2)}>
+            {page + 2}
+          </Pagination.Item>
+          <Pagination.Item key={page + 3} onClick={() => setPage(page + 3)}>
+            {page + 3}
+          </Pagination.Item>
+          <Pagination.Next key={'next'} onClick={() => setPage(page + 1)} />
+          <Pagination.Last key={'last'} onClick={() => setPage(lastPage)} />
+        </>
+      );
+      setPaginationItems(items);
+    }
+    if (page >= 3 && page <= lastPage - 2) {
+      const items = (
+        <>
+          <Pagination.First key={'first'} onClick={() => setPage(1)} />
+          <Pagination.Prev key={'prev'} onClick={() => setPage(page - 1)} />
+          <Pagination.Item key={page - 2} onClick={() => setPage(page - 2)}>
+            {page - 2}
+          </Pagination.Item>
+          <Pagination.Item key={page - 1} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Pagination.Item>
+          <Pagination.Item key={page} active>
+            {page}
+          </Pagination.Item>
+          <Pagination.Item key={page + 1} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Pagination.Item>
+          <Pagination.Item key={page + 2} onClick={() => setPage(page + 2)}>
+            {page + 2}
+          </Pagination.Item>
+          <Pagination.Next key={'next'} onClick={() => setPage(page + 1)} />
+          <Pagination.Last key={'last'} onClick={() => setPage(lastPage)} />
+        </>
+      );
+      setPaginationItems(items);
+    }
+    if (page === lastPage - 1) {
+      const items = (
+        <>
+          <Pagination.First key={'first'} onClick={() => setPage(1)} />
+          <Pagination.Prev key={'prev'} onClick={() => setPage(page - 1)} />
+          <Pagination.Item key={page - 3} onClick={() => setPage(page - 3)}>
+            {page - 3}
+          </Pagination.Item>
+          <Pagination.Item key={page - 2} onClick={() => setPage(page - 2)}>
+            {page - 2}
+          </Pagination.Item>
+          <Pagination.Item key={page - 1} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Pagination.Item>
+          <Pagination.Item key={page} active>
+            {page}
+          </Pagination.Item>
+          <Pagination.Item key={page + 1} onClick={() => setPage(page + 1)}>
+            {page + 1}
+          </Pagination.Item>
+          <Pagination.Next key={'next'} onClick={() => setPage(page + 1)} />
+          <Pagination.Last key={'last'} onClick={() => setPage(lastPage)} />
+        </>
+      );
+      setPaginationItems(items);
+    }
+    if (page === lastPage) {
+      const items = (
+        <>
+          <Pagination.First key={'first'} onClick={() => setPage(1)} />
+          <Pagination.Prev key={'prev'} onClick={() => setPage(page - 1)} />
+          <Pagination.Item key={page - 4} onClick={() => setPage(page - 4)}>
+            {page - 4}
+          </Pagination.Item>
+          <Pagination.Item key={page - 3} onClick={() => setPage(page - 3)}>
+            {page - 3}
+          </Pagination.Item>
+          <Pagination.Item key={page - 2} onClick={() => setPage(page - 2)}>
+            {page - 2}
+          </Pagination.Item>
+          <Pagination.Item key={page - 1} onClick={() => setPage(page - 1)}>
+            {page - 1}
+          </Pagination.Item>
+          <Pagination.Item key={page} active>
+            {page}
+          </Pagination.Item>
+          <Pagination.Next key={'next'} disabled />
+          <Pagination.Last key={'last'} disabled />
+        </>
+      );
+      setPaginationItems(items);
+    }
+  }, [page, lastPage]);
 
   return (
-    <div>
+    <div className="list-page">
       <h2>NHL Players</h2>
-      {console.log(players)}
       <PlayerList />
       <Pagination>{paginationItems}</Pagination>
     </div>
--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -3,9 +3,9 @@ import { client, checkError } from './client.js';
 export async function getPlayerData(from, to) {
   const response = await client
     .from('nhl_players')
-    .select('*')
+    .select('*', { count: 'exact' })
     .gte('games_played', 10)
     .range(from, to);
 
-  return checkError(response);
+  return { data: response.data, count: response.count };
 }
